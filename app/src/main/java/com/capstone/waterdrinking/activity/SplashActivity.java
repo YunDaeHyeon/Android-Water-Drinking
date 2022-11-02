f02eeb074e313ef54176b091887b715d3c012664
SONG Yugyeong <syk9471@naver.com>
2022-11-02 20:21:56 +0900

    22.11.02 feature : 디자인 추기

--- app/src/main/java/com/capstone/waterdrinking/activity/SplashActivity.java
+++ app/src/main/java/com/capstone/waterdrinking/activity/SplashActivity.java
@@ -5,15 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.capstone.waterdrinking.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private TextView startTextView;
+    private ImageView startWater;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        startTextView = (TextView) findViewById(R.id.startTextView);
+        startWater = (ImageView) findViewById(R.id.startWater);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha);
+        startTextView.startAnimation(animation);
+        startWater.startAnimation(animation);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -23,6 +36,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 2000); // 2초 뒤 메인화면 이동
+        }, 3000); // 2초 뒤 메인화면 이동
     }
 }
